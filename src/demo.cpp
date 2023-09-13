1b2fec20741a4ea4f79d94804a4f7911d622b652
Jake Keller <jakeller3630@gmail.com>
2023-09-13 16:53:05 -0500

    Implement programmatic stopping of k-means iteration

--- src/demo.cpp
+++ src/demo.cpp
@@ -10,6 +10,7 @@
 #include "xtensor/xcsv.hpp"
 #include "xtensor/xrandom.hpp"
 #include "xtensor/xbuilder.hpp"
+#include "xtensor/xmath.hpp"
 #include "xtensor/xaxis_iterator.hpp"
 #include "xtensor-blas/xlinalg.hpp"
 #include "matplot/matplot.h"
@@ -113,21 +114,23 @@ void plotData(xt::xarray<double> observations, xt::xarray<double> groups, xt::xa
 
 xt::xarray<double> computeKMeans(
     xt::xarray<double> observations,
-    xt::xarray<double> VTxyz
+    xt::xarray<double> VTxyz,
+    unsigned max_iters
 )
 {
     xt::xarray<double> points = xt::linalg::dot(observations, xt::transpose(VTxyz));
 
     size_t point_count = points.shape()[0];
     xt::xarray<double> groups = xt::empty<double>(std::vector<size_t>{point_count, 1});
+    xt::xarray<double> old_groups = groups;
 
     xt::xtensor_fixed<double, xt::xshape<1, 3>> k0, k1, p;
-    // initialize cluster centers
-    k0 = xt::view(points, xt::range(0, 1), xt::all());
-    k1 = xt::view(points, xt::range(1, 2), xt::all());
+    // initialize cluster centers -- TODO: randomize
+    k0 = xt::view(points, xt::range(2, 3), xt::all());
+    k1 = xt::view(points, xt::range(8, 9), xt::all());
 
     // TODO: remove hardcoded # of iterations
-    for (auto i = 0; i < 10; ++i) {
+    for (unsigned i = 0; i < max_iters; ++i) {
         for (size_t i = 0; i < point_count; ++i) {
             p = xt::view(points, xt::range(i, i + 1), xt::all());
             if (xt::linalg::norm(k0 - p) < xt::linalg::norm(k1 - p)) {
@@ -136,6 +139,10 @@ xt::xarray<double> computeKMeans(
                 groups(i) = 1.0;
             }
         }
+
+        if (groups == old_groups) break;
+        old_groups = groups;
+
         // TODO: difficult to parse, split into its own fn (lambda?)
         k0 = xt::mean(xt::view(points, xt::drop(xt::where(groups > 0.5)[0]), xt::all()), 0);
         k1 = xt::mean(xt::view(points, xt::keep(xt::where(groups > 0.5)[0]), xt::all()), 0);
@@ -147,20 +154,27 @@ xt::xarray<double> computeKMeans(
 int main()
 {
     xt::random::seed(80085);
+    //xt::random::seed(1337);
 
     auto [train_obs, train_grp, test_obs, test_grp] =
         readData("data/ovariancancer_obs.csv", "data/ovariancancer_grp.csv");
 
     // Normalize the data and calculate the SVD
     // TODO: the xt::stddev call is slow... consider replacing?
+    std::cout << "TODO: xt::stddev is slow, consider replacing" << std::endl;
     xt::xarray<double> X = (train_obs - xt::mean(train_obs, 0)) / xt::stddev(train_obs, {0});
+    std::cout << "xt::stddev finished" << std::endl;
+
     auto [U, S, VT] = xt::linalg::svd(X, false);
     // Use the first three principal components
     auto VTxyz = xt::view(VT, xt::range(0, 3), xt::all());
     plotData(X, train_grp, VTxyz);
 
-    auto kmeans_grp = computeKMeans(X, VTxyz);
+    auto kmeans_grp = computeKMeans(X, VTxyz, 100);
     plotData(X, kmeans_grp, VTxyz);
+
+    auto error = xt::sum(xt::cast<int>(train_grp) ^ xt::cast<int>(kmeans_grp))();
+    std::cout << "Number of misclassified patients: " << error << std::endl;
 
     return 0;
 }
